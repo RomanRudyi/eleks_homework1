f2b0275d3977f0b209b95d7adf9c52f4ceb427f9
RomanRudyi <roma1995101@gmail.com>
2017-03-15 17:50:53 +0200

    Added Task 3 & Task 4

---
+++ Task3/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[5, 5]
+            {
+                { 4, 5, 5, 5, -5 },
+                { 1, 2, -3, 10, -4 },
+                { 1, 4, 3, 4, -5 },
+                { 13, 11, 11, 6, 3 },
+                { 2, 9, 3, 8, 1 }
+            };
+
+            Console.WriteLine("Sum in row with negative numbers");
+            SumInRowNeg(matrix);
+
+            ShowSaddlePoints(matrix);
+
+            Console.Read();
+        }
+
+        #region Sum in row with negative numbers
+        public static void SumInRowNeg(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int sum = 0;
+                bool hasNegative = false;
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] < 0)
+                        hasNegative = true;
+                    sum += matrix[i, j];
+                }
+                if (hasNegative)
+                    Console.Write($"Sum int row {i} = {sum}\n");
+            }
+        }
+        #endregion
+
+        #region Saddle point
+        public static void ShowSaddlePoints(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (IsMinInRow(matrix, i, j) && IsMaxInCol(matrix, i, j))
+                        Console.WriteLine($"Saddle point: row = {i}, column = {j}");
+                }
+            }
+        }
+
+        private static bool IsMinInRow(int[,] matrix, int row, int col)
+        {
+            bool isMin = true;
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                if (i == col) continue;
+                if (matrix[row, col] > matrix[row, i])
+                    isMin = false;
+            }
+            return isMin;
+        }
+
+        private static bool IsMaxInCol(int[,] matrix, int row, int col)
+        {
+            bool isMax = true;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                if (i == row) continue;
+                if (matrix[row, col] < matrix[i, col])
+                    isMax = false;
+            }
+            return isMax;
+        }
+        #endregion
+    }
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[,]
+            {
+                { 1, 1, 1, 1, 1, 1, 1, 1 },
+                { 1, 2, 2, 2, 2, 2, 2, 2 },
+                { 1, 2, 3, 4, 2, 6, 7, 8 },
+                { 1, 2, 3, 4, 2, 6, 7, 8 },
+                { 1, 2, 2, 2, 2, 2, 2, 2 },
+                { 1, 2, 3, 4, 2, 6, 7, 8 },
+                { 1, 2, 3, 4, 2, 6, 7, 8 },
+                { 1, 2, 3, 4, 2, 6, 7, 8 }
+            };
+
+            int[,] matrix4 = new int[,]
+            {
+                { 1, 1, 1, 1 },
+                { 1, 2, 2, 2 },
+                { 1, 2, 3, 4 },
+                { 1, 2, 3, 4 }
+            };
+
+            Console.WriteLine("Row & col are equals");
+
+            ShowEqualsRowAndCol(matrix);
+
+            Console.Read();
+        }
+
+        public static void ShowEqualsRowAndCol(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                bool areEqual = true;
+                for (int j = 0; j < matrix.GetLength(0); j++)
+                {
+                    if (matrix[i, j] != matrix[j, i])
+                        areEqual = false;
+                }
+                if (areEqual)
+                    Console.WriteLine($"k = {i}");
+            }
+        }
+    }
+}
